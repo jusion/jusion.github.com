83fa7c9ffb014665c47faaa2ccf9edc894870803
jusion@gmail.com <jusion@gmail.com>
2012-07-24 10:16:45 -0400

    Test jump fix

--- game/js/player.js
+++ game/js/player.js
@@ -24,7 +24,7 @@ Crafty.c("Player", {
 	attack: false,
 	shoot: false,
 	blink: false,
-	jumpDist: 40,
+	jumpDist: 60,
 	facing: "down",
 	hurt: false,
 	init: function() {
@@ -147,12 +147,12 @@ Crafty.c("Player", {
 		//this.stop().animate("blink", 10, -1);
 		
 		if(this.facing === "up") {
-			if((this.y + Crafty.viewport.y) >= 320){
-				this.jumpDist = this.y - Crafty.viewport.y - 18;
-			} else if((this.y + Crafty.viewport.y) <= 0){
-				this.jumpDist = Crafty.viewport.y - this.y + 18;
+			theJump = this.y + Crafty.viewport.y - 18
+			if(theJump <= this.jumpDist){
+				this.y -= theJump;
+			} else {
+				this.y -= this.jumpDist;
 			}
-			this.y -= this.jumpDist;
 		} else if (this.facing === "down") {
 			if((this.y + Crafty.viewport.y) >= 320){
 				this.jumpDist = this.y - Crafty.viewport.y - 18;
