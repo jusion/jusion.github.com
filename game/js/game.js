ecb5c1e474b8c2b2352a9e2f97d75b25b4f5825f
jusion@gmail.com <jusion@gmail.com>
2012-07-26 09:55:57 -0400

    Fix errorS?

--- game/js/game.js
+++ game/js/game.js
@@ -123,26 +123,25 @@ window.onload = function() {
 	});
 	
 	Crafty.audio.add({
-		swing1: ["game/sfx/swing.mp3","game/sfx/swing.ogg"],
-		swing2: ["game/sfx/swing2.mp3","game/sfx/swing2.ogg"],
-		swing3: ["game/sfx/swing3.mp3","game/sfx/swing3.ogg"],
-		spell: ["game/sfx/spell.mp3","game/sfx/spell.ogg"],
+		swing1: ["game/sfx/swing.mp3"],
+		swing2: ["game/sfx/swing2.mp3"],
+		swing3: ["game/sfx/swing3.mp3"],
+		spell: ["game/sfx/spell.mp3"],
 		jump: ["game/sfx/jump.mp3","game/sfx/jump.ogg"],
 
 		boss1: ["game/sfx/boss1.mp3","game/sfx/boss1.ogg"], 
 		boss2:["game/sfx/boss2.mp3","game/sfx/boss2.ogg"], 
 		boss3:["game/sfx/boss3.mp3","game/sfx/boss3.ogg"], 
 		enemy:["game/sfx/enemy.mp3","game/sfx/enemy.ogg"], 
-		benemy:["game/sfx/benemy.mp3","game/sfx/benemy.ogg"],
+		benemy:["game/sfx/benemy.mp3"],
 
-		title:["game/sfx/title.mp3","game/sfx/title.ogg"],
-		cave:["game/sfx/cave.mp3","game/sfx/cave.ogg"],
-		desert:["game/sfx/desert.mp3", "game/sfx/desert.ogg"]
+		title:["game/sfx/title.mp3"],
+		cave:["game/sfx/cave.mp3"],
+		desert:["game/sfx/desert.mp3",]
 	});
 
 	Crafty.audio.add("default",
-					["game/sfx/default.mp3",
-					"game/sfx/default.ogg"]);
+					["game/sfx/default.mp3"]);
 	// inital function to generate all the entities
 	function generateEnts() {
 		// Player init
